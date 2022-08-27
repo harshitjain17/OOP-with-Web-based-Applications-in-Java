df339f8762ca184b89d1cda9c4c4c72c91a72d40
Harshit Jain <harshitj.cs@gmail.com>
2022-08-27 19:05:33 -0400

    init array

---
+++ JavaPractice/src/javapractice/InitArray.java
@@ -0,0 +1,26 @@
+package javapractice;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/* @author Harshit Jain */
+public class InitArray {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // declare variable array and initialize it with an array object
+        int[] array = new int[10]; // create the array object
+        
+        System.out.printf("%s%8s%n", "Index", "Value"); // column headings
+        
+        // outpt each array element's value
+        for (int counter = 0; counter < array.length; counter++ ) {
+            System.out.printf("%3d%8d%n", counter, array[counter]);
+        }
+    }
+
+}

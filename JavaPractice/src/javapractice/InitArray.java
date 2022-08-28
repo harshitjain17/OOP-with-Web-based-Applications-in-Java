c14c92029e4606faf86855ded325278ff680a74a
Harshit Jain <harshitj.cs@gmail.com>
2022-08-27 22:06:34 -0400

    array

--- JavaPractice/src/javapractice/InitArray.java
+++ JavaPractice/src/javapractice/InitArray.java
@@ -14,13 +14,18 @@ public class InitArray {
         
         // declare variable array and initialize it with an array object
         int[] array = new int[10]; // create the array object
+        int total = 0;
         
         System.out.printf("%s%8s%n", "Index", "Value"); // column headings
         
         // outpt each array element's value
         for (int counter = 0; counter < array.length; counter++ ) {
             System.out.printf("%3d%8d%n", counter, array[counter]);
+            total += array[counter]; // add each element value to total
         }
+        
+        // printing total
+        System.out.printf("Total of array elements: %d", total);
     }
 
 }
